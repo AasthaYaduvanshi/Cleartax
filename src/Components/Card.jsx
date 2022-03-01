983e7f6cffab15e7659397b7e513b004388c1fee
Your Name <you@example.com>
2022-03-01 10:54:01 +0530

    hover effect in cards and resized images

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,9 +10,9 @@ const Card = props => {
             </div>
             <div className="card-body">
                 <h4 className="card-title">Card Title</h4>
-                <p className="card-text text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aperiam culpa iste, 
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aperiam culpa iste, 
                 eaque perspiciatis doloremque deserunt natus minima vitae, non mollitia tenetur. Tenetur ut accusantium qui, at eos exercitationem architecto!</p>
-                <a href="#" className="btn btn-outline success">See Products </a>
+                <a href="#" className="BTN-Text">See Products<span className="Arrow">&nbsp; →</span> </a>
             </div>
         </div>
     )
